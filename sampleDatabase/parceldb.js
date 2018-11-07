d3bb6c6e5461286f9317761e2334f0341c1dc472
bobbylemm <chidozienwoga@gmail.com>
2018-11-07 22:49:21 +0100

    created sample database for parcels

---
+++ sampleDatabase/parceldb.js
@@ -0,0 +1,27 @@
+const parcels = [
+    {
+        id: 1,
+        packageName: "fridge",
+        destination: "lagos",
+        pickupLocation: "iyana ipaja",
+        status: 'enroute',
+        price: "45000 naira"
+    },
+    {
+        id: 2,
+        packageName: "television",
+        destination: "ibadan",
+        pickupLocation: "ile-ife",
+        status: 'enroute',
+        price: "25000 naira"
+    },
+    {
+        id: 3,
+        packageName: "car",
+        destination: "imo",
+        pickupLocation: "wetheral",
+        status: 'being packaged',
+        price: "180,000 naira"
+    }
+];
+export default parcels;
