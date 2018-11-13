449bdbbc0da3165f19a9c1cf90461ff86926e037
bobbylemm <chidozienwoga@gmail.com>
2018-11-13 22:33:38 +0100

    feat(add-map): added a google map to enable users track their orders and aslo input markers [Delivers #161938274]

--- UI/track deliveries/trackDeliveries.js
+++ UI/track deliveries/trackDeliveries.js
@@ -1,7 +1,15 @@
 initMap = () => {
         let options = {
-                zoom: 8,
-                center: {lat: 6.5244, lng: -3.3792}
+                zoom: 10,
+                center: {lat: 6.5244, lng: 3.3792}
         }
-        const map = new google.maps.Map(document.getElementById('map'), options)
+        const map = new google.maps.Map(document.getElementById('map'), options);
+        // add the markers
+        const marker = new google.maps.Marker({
+                position: {
+                        lat: 6.5244,
+                        lng: 3.3792
+                },
+                map,
+        })
 }
