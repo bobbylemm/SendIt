66fd5985ec633f72d31aa2ef0b884066a614cb9a
bobbylemm <chidozienwoga@gmail.com>
2018-11-14 22:58:11 +0100

    modified the profile page and the view all page, also made buttons larger, and implemented all UI feedback

--- UI/admin page/admin.js
+++ UI/admin page/admin.js
@@ -1,5 +1,4 @@
-const editBtn = document.querySelectorAll('#editBtn');
-const modal = document.getElementById('modal');
+const modal = document.getElementById('modal-admin-edit');
 const close = document.getElementById('close');
 
 const edit = () => {
--- UI/profile page/profile.js
+++ UI/profile page/profile.js
@@ -2,6 +2,9 @@ const parcelsDelivered = document.getElementById('p-deliv');
 const viewAllParcels = document.getElementById('view-all');
 const contentOne = document.getElementById('btn-one-ft');
 const contentTwo = document.getElementById('btn-two-ft');
+const modalEdit = document.getElementById('modal-edit');
+const modalCancel = document.getElementById('modal-cancel');
+const close = document.getElementById('close');
 
 parcelsDelivered.addEventListener('click', () => {
         contentOne.style.display = 'block';
@@ -11,3 +14,17 @@ viewAllParcels.addEventListener('click', () => {
         contentOne.style.display = 'none';
         contentTwo.style.display = 'block';
 })
+// js for the modal
+const edit = () => {
+        modalEdit.style.display = 'block';
+}
+close.addEventListener('click', () => {
+        modalEdit.style.display = 'none';
+})
+// 
+const cancel = () => {
+        modalCancel.style.display = 'block';
+}
+const noCancel = () => {
+        modalCancel.style.display = 'none';
+}
