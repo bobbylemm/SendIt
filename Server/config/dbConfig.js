833202fa4bb7d4773f737812bb96400017e5289b
bobbylemm <chidozienwoga@gmail.com>
2018-11-18 20:31:10 +0100

    added a config file with database connection and also created a .env file with enviroment variables

---
+++ Server/config/dbConfig.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const databaseConfig = {
+  development: {
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    port: '5432',
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    max: 10
+  },
+  test: {
+    user: process.env.DATABASE_TEST_USER,
+    host: process.env.DATABASE_TEST_HOST,
+    port: '5432',
+    database: process.env.DATABASE_TEST_NAME,
+    password: process.env.DATABASE_TEST_PASSWORD,
+    max: 10
+  },
+  production: {
+    user: process.env.DATABASE_URL,
+    host: process.env.DATABASE_URL,
+    port: process.env.DATABASE_URL,
+    database: process.env.DATABASE_URL,
+    password: process.env.DATABASE_URL
+  }
+};
+export default databaseConfig;
