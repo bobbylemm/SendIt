66eecb6389a7c9f4d7d7240dc6b6daac34ffb0d4
bobbylemm <chidozienwoga@gmail.com>
2018-12-19 11:16:12 +0100

    fixing heroku error

--- UI/client/admin.js
+++ UI/client/admin.js
@@ -4,7 +4,7 @@ const modalEdit = document.querySelector('#modal-edit');
 const modalMessage = document.querySelector('#modal-message');
 const loader = document.querySelector('#loader');
 
-const url1 = `http://localhost:3000/api/v1/parcels`;
+const url1 = `/api/v1/parcels`;
 
 if(!token) {
     window.location.replace('/');
@@ -102,7 +102,7 @@ const edit = (e) => {
             const PresentLoca = presentlocation.innerHTML;
             const splitPresentLoca = PresentLoca.split('&');
             const updatedPresentLoca = splitPresentLoca[0];
-            const url2 = `http://localhost:3000/api/v1/parcels/${pid}/status`;
+            const url2 = `/api/v1/parcels/${pid}/status`;
             fetch(url2, {
                 method: 'PUT',
                 body: JSON.stringify({
@@ -142,6 +142,5 @@ const edit = (e) => {
             .catch(err => console.log(err))
             })
             .catch(err => console.log(err))
-            // update the present location
         }
     }
